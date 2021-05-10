9110ecd2cedf22109f5d6c8592c5f116c8f8b14f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 10:35:51 +0300

    Rokas

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,10 +8,15 @@ const Search = () => {
     return (
         <div className="search">
             <div className="search__input">
-                <SearchIcon />
+                <SearchIcon className="search__inputIcon" />
                 <input type="text"/>
-                <MicIcon />
+                <MicIcon className="search__inputIcon" />
 
+            </div>
+
+            <div className="search__buttons">
+                <Button variant="outlined">Rokas Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
             
         </div>
