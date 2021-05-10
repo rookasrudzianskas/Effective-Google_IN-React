e651a6471ecf37244112424e0a4055a6c53634e5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 09:27:24 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hello 🔥</h1>
+        <Home />
     {/*    Home page (the one with the search on) */}
 
     {/*    Search page (the results page) */}
