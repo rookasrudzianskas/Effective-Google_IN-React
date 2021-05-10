609b30ec21dd70a8dd93a8ca21befd5b9b81e024
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 10:12:20 +0300

    Rokas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
             </div>
 
             <div className="home__body">
-
+                <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
             </div>
 
         </div>
