b9da94cbe2a9fc9debfaca084336160753e338c0
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 16:17:43 +0300

    Rokas

--- src/hooks/useGoogleSearch.js
+++ src/hooks/useGoogleSearch.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import API_KEY from "../keys";
 
-const useGoogleSearch = () => {
-    return (
-        <div className="useGoogleSearch">
-            
-        </div>
-    );
+// it tells google which search engine to use
+const CONTEXT_KEY = "41d3eac0d47fdca20";
+
+const useGoogleSearch = (term) => {
+    // custom hook
+    // this hook is going to have it is own piece of state to store the data
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        // we pass the term, and then the term changes, we fire this code
+
+    }, [term])
 };
 
 export default useGoogleSearch;
