af668c204c0b53211e99db8a6074713031bc17e1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 09:19:49 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="app">
       <h1>Hello 🔥</h1>
+    {/*    Home page (the one with the search on) */}
+
+    {/*    Search page (the results page) */}
     </div>
   );
 }
