9b8e1eb625fb3ba0926b97b9c19f22c636b92881
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 09:38:22 +0300

    Rokas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>Hello</h1>
+            <div className="home__header">
+                <div className="home__headerLeft">
+                {/*     Link    */}
+                {/*     Link    */}
+                </div>
+
+                <div className="home__headerRight">
+                {/*    Link */}
+                {/*    Link */}
+                {/*    ICon */}
+                {/*    Avatar */}
+                </div>
+
+            </div>
+
+            <div className="home__body">
+
+            </div>
+
         </div>
     );
 };
