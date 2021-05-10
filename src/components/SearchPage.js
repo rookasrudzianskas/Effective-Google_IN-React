485602bef9e4088e5498af2f349638d59cff1a21
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 16:14:10 +0300

    Rokas

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -5,11 +5,11 @@ import {useStateValue} from "../StateProvider";
 const SearchPage = () => {
     // getting data from data layer
     const [{ term }, dispatch] = useStateValue();
-
+    // we get an api key from here
     //https://developers.google.com/custom-search/v1/using_rest
-
+    // we register the search to search worldwide here
     //https://cse.google.com/cse/create/new
-
+    //41d3eac0d47fdca20
     return (
         <div className="searchPage">
             <div className="searchPage__header">
---
+++ src/hooks/useGoogleSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const useGoogleSearch = () => {
+    return (
+        <div className="useGoogleSearch">
+            
+        </div>
+    );
+};
+
+export default useGoogleSearch;
