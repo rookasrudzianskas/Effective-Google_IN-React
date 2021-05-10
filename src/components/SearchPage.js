db876293e332b132e13fe0a294212b1adb48f093
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 16:28:04 +0300

    Rokas

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -7,12 +7,15 @@ const SearchPage = () => {
     // getting data from data layer
     const [{ term }, dispatch] = useStateValue();
     // we call the hook google search to make the request, and we put the term, from the data layer here
-    const {data} = useGoogleSearch(term)
+    const {data} = useGoogleSearch(term);
+
     // we get an api key from here
     //https://developers.google.com/custom-search/v1/using_rest
+
     // we register the search to search worldwide here
     //https://cse.google.com/cse/create/new
     //41d3eac0d47fdca20
+
     return (
         <div className="searchPage">
             <div className="searchPage__header">
