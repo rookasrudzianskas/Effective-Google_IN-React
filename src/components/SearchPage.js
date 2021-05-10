0224bc320dba050f08601676402aa352ff5a90b4
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 15:58:37 +0300

    Rokas

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./styles/SearchPage.css";
+import {useStateValue} from "../StateProvider";
 
 const SearchPage = () => {
+    const [{ term }, dispatch] = useStateValue();
+
     return (
         <div className="searchPage">
+            <div className="searchPage__header">
+                <h1>{term}</h1>
 
+            </div>
+
+            <div className="searchPage__results">
+
+            </div>
         </div>
     );
 };
