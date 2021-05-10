368b8a19261523d0d6c620804ebb2ca62e754ba8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 09:42:28 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-        <Home />
-    {/*    Home page (the one with the search on) */}
+        <Router>
+            <Switch>
+                <Route path="/search">
+                    <h1>This is earch</h1>
 
-    {/*    Search page (the results page) */}
+                    {/*    Search page (the results page) */}
+                </Route>
+
+                <Route path="/">
+                    <h1>This is home</h1>
+
+                    <Home />
+                    {/*    Home page (the one with the search on) */}
+                </Route>
+            </Switch>
+
+        </Router>
     </div>
   );
 }
